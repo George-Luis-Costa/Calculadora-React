fc57ab2c3c27fe61e33aa92a2c9363576307086e
George Luis Costa Ribeiro <georgel.jc98@gmail.com>
2021-12-16 12:17:57 -0300

    First

---
+++ src/components/Button.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import './Button.css'
+
+export default props => {
+    //2 forma
+    //Exibicao condicional de classes
+    let classes = 'button '  // Espaço no final para concatenar com as outras string, como 'button' sempre vai ser definido.
+    classes += props.operation ? 'operation' : '' //Aqui há condiçoes: se a props estiver definida (true), coloca-se a classe "operation", caso contrario " " (vazio). Esse vazio vai servir para concatenar as 3 props(caso ative as 3) em strings
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button
+                        // para garantir
+            onClick={e => props.click && props.click(e.target.innerHTML)} // Para testar, ao clicar no botton, mostrar o conteudo de cada botao no console
+            className={classes}>   {/*Passando as classes para se estilizar  */}
+            {props.label}
+        </button>
+    )
+}
+
+// 1 forma
+// Com template String, e ${ } para script JS nativo
+    // < button className = {`
+    //     button
+    //     ${props.operation ? 'operation' : ''}
+    //     ${props.operation ? 'double' : ''}
+    //     ${props.operation ? 'triple' : ''}
+    
+    // `}>
+    // { props.label }
+    // </button >
+
